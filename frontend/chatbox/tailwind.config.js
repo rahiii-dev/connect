00cf7bb005b2c10a10300878bcfc47200b3fca67
rahiii-dev <rahiofcl@gmail.com>
2024-09-28 13:15:51 +0530

    frontend setup

---
+++ frontend/chatbox/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '100%',
+          md: '720px',
+          lg: '960px',
+          xl: '1140px',
+          '2xl': '1320px',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
